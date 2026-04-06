45dbd671e0b2f53bb632112f07f92baa20da0d5f
Rejzzsz <jeromyjerikho@gmail.com>
2026-04-06 21:04:41 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-blue-600">
+        Welcome
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
